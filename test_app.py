7bc81a7709016d776b5ca0fc93ee1a2f35d16522
Vatsala <vatsala.vaddiraju@gmail.com>
2021-09-26 10:18:55 +0530

    test.py change for codecov

--- test_app.py
+++ test_app.py
@@ -2,6 +2,8 @@ from fastapi.testclient import TestClient
 from main import app
 
 # test to check the correct functioning of the /ping route
+
+
 def test_ping():
     with TestClient(app) as client:
         response = client.get("/ping")
@@ -24,3 +26,29 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# test to check if Iris Virginica is classified correctly
+
+
+def test_feedbackloop():
+    # defining a sample payload for the testcase
+    payload = [{
+        "sepal_length": 7.0,
+        "sepal_width": 3.2,
+        "petal_length": 4.7,
+        "petal_width": 1.4,
+        "flower_class": "Iris Versicolour"
+    },
+        {
+        "sepal_length": 5.4,
+        "sepal_width": 3.9,
+        "petal_length": 1.7,
+        "petal_width": 0.4,
+        "flower_class": "Iris Setosa"
+    }]
+
+    with TestClient(app) as client:
+        response = client.post("/feedback_loop", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"detail": "Feedback loop successful"}
